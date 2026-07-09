1de1fb00da789e72e7971984283ae7db407222ee
Volodymyr Suskyi <suskyivolodymyr@gmail.com>
2026-07-09 16:56:08 +0300

    fix(i18n): move negotiateLocale out of the use-server module

--- src/i18n/locale.test.ts
+++ src/i18n/locale.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { negotiateLocale } from "./locale";
+import { negotiateLocale } from "./negotiate";
 import { isRtl } from "./config";
 
 describe("negotiateLocale", () => {
